032503d592a99caa678df9269a99ccea09229657
DenisGoldiner <goldiner19@gmail.com>
2022-09-18 16:59:08 +0300

    SL-0039: template for table tests

--- integration/create_booking_test.go
+++ integration/create_booking_test.go
@@ -2,37 +2,64 @@ package integration
 
 import (
 	"context"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
-func Test_integration_CreateBooking_ok(t *testing.T) {
-	dbExec := setupDB(t)
+func Test_integration_CreateBooking_requestValidation(t *testing.T) {
+	t.Parallel()
 
-	body := strings.NewReader("{}")
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, bookingsURL, body)
-	require.NoError(t, err)
-	recorder := httptest.NewRecorder()
+	testCases := map[string]struct {
+		body           io.Reader
+		expResponseErr string
+	}{
+		"no_body": {
+			body:           http.NoBody,
+			expResponseErr: fmt.Sprintln("there is no request body, but it is expected"),
+		},
+		//"empty_first_name":     {},
+		//"empty_last_name":      {},
+		//"empty_birthday":       {},
+		//"empty_gender":         {},
+		//"not_supported_gender": {},
+		//"empty_launchpad_id":   {},
+		//"empty_launch_date":    {},
+		//"empty_destination":    {},
+	}
+
+	// we won't reach the DB as all cases will fail the validation.
+	dbExec := setupDB(t)
 	router := newTestRouter(dbExec)
 
-	router.ServeHTTP(recorder, req)
+	for tcName, tc := range testCases {
+		tc := tc
 
-	require.Equal(t, http.StatusNoContent, recorder.Code)
+		t.Run(tcName, func(t *testing.T) {
+			req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, bookingsURL, tc.body)
+			require.NoError(t, err)
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, req)
+
+			require.Equal(t, http.StatusBadRequest, recorder.Code)
+			require.Equal(t, tc.expResponseErr, recorder.Body.String())
+		})
+	}
 }
 
-//func Test_integration_CreateBooking_emptyBody(t *testing.T) {
-//	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, "/bookings", http.NoBody)
+//func Test_integration_CreateBooking_ok(t *testing.T) {
+//	dbExec := setupDB(t)
+//
+//	body := strings.NewReader("{}")
+//	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, bookingsURL, body)
 //	require.NoError(t, err)
 //	recorder := httptest.NewRecorder()
-//	router := newTestRouter(t)
-//	expectedErr := fmt.Sprintln("there is no request body, but it is expected")
+//	router := newTestRouter(dbExec)
 //
 //	router.ServeHTTP(recorder, req)
 //
-//	require.Equal(t, http.StatusBadRequest, recorder.Code)
-//	require.Equal(t, expectedErr, recorder.Body.String())
+//	require.Equal(t, http.StatusNoContent, recorder.Code)
 //}
