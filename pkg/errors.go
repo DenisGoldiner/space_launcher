702c64897307e797ea579174a62d92d746960998
DenisGoldiner <goldiner19@gmail.com>
2022-09-18 21:45:18 +0300

    SL-0047: improve supporting packages

--- pkg/errors.go
+++ pkg/errors.go
@@ -2,10 +2,9 @@ package pkg
 
 import "fmt"
 
-const (
-	errWrapFILOFormat = "%s; %w"
-)
+const errWrapFILOFormat = "%s; %w"
 
+// WrapErr formatted FILO wrap for errors.
 func WrapErr(cause string, err error) error {
 	return fmt.Errorf(errWrapFILOFormat, cause, err)
 }
